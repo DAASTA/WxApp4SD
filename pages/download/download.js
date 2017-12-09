aaadce28244ca2f66f86dfc9413023e611162c1a
RunTimeError2 <songjiahao980530@126.com>
2017-12-09 09:36:48 +0800

    12.9 version, first commit

---
+++ pages/download/download.js
@@ -0,0 +1,234 @@
+var iscomplete = false
+var original_queue_number = -2
+var waiting_in_queue = true
+var add_step = 0.5
+var percent_sup = 0
+var add_percent = false
+var progress_percent = 10
+var is_uploading = true
+
+Page({
+  data: {
+    src: '',
+    imgsrc: "",
+    result_src: "",
+    getresult: false,
+    waiting_num: -1,
+    notification_text: "",
+    progressPercent: 10
+  },
+  onLoad(option) {
+    var that = this
+    wx.getUserInfo({
+      success: function (res) {
+        var userInfo = res.userInfo
+        that.setData({ imgsrc: userInfo.avatarUrl })
+      }
+    })
+
+    is_uploading = true
+    that.setData({ notification_text: "正在上传文件" })
+    add_percent = true
+    percent_sup = 20
+    //上传文件
+    console.log("ready to upload file!")
+    wx.uploadFile({
+      url: "https://" + getApp().globalData.core_url + "/upload/",
+      filePath: getApp().globalData.editedImgSrc,
+      name: 'data',
+      header: {
+        'content-type': "Application/json"
+      },
+      formData: {
+        id: getApp().globalData.user_ID,
+        style: getApp().globalData.selectedStyle
+      },
+      success: function (res) {
+        is_uploading = false
+        add_percent = false
+        console.log("upload file done.")
+      },
+      failure: function (res) {
+        console.log("failure!")
+        console.log(res)
+        wx.showToast({
+          title: "Error!无法连接到服务器",
+          icon: "error",
+          duration: 2000
+        })
+      }
+    })
+
+    wx.request({
+      url: "https://" + getApp().globalData.core_url + "/query/",
+      data: {
+        id: getApp().globalData.user_ID
+      },
+      method: 'GET',
+      success: function(res) {
+        if(res.data == 0)
+          that.setData({progressPercent: 40})
+      }
+    })
+
+    setInterval(function() {
+      if(add_percent && progress_percent <= percent_sup) {
+        progress_percent = progress_percent + add_step
+        that.setData({progressPercent: progress_percent})
+      }
+    }, 100)
+
+    setInterval(function () {
+      if(is_uploading) {
+        that.setData({ notification_text: "正在上传文件"})
+        percent_sup = 20
+        add_percent = true
+      }
+      if (!iscomplete && !is_uploading) {
+        wx.request({
+          url: "https://" + getApp().globalData.core_url + "/query/",
+          data: {
+            id: getApp().globalData.user_ID
+          },
+          method: 'GET',
+          success: function (res) {
+            that.setData({waiting_num: res.data})
+            console.log("get queue info, " + res.data)
+            if(original_queue_number == -2)
+              original_queue_number = res.data
+            if(res.data > 0)
+              waiting_in_queue = true
+            else
+              waiting_in_queue = false
+            if(waiting_in_queue) {
+              if(res.data == 0) {
+                that.setData({progressPercent: 40})
+                progress_percent = 40
+              }
+              else
+                if(original_queue_number > 0 && res.data > 0) {
+                  var currentprecent = 40 - 20 * res.data / original_queue_number
+                  that.setData({progressPercent: currentprecent})
+                  progress_percent = currentprecent
+                }
+            }
+            if(res.data == 0) {
+              that.setData({notification_text: "正在处理您提交的图片，请稍后..."})
+              if(progress_percent < 40) {
+                progress_percent = 40
+                that.setData({progressPercent: progress_percent})
+              }
+              add_percent = true
+              percent_sup = 85
+            }
+            else  
+              if(res.data > 0) {
+                that.setData({notification_text: "前面还有" + res.data + "人在排队，请耐心等候"})
+                add_percent = false
+                console.log("waiting in queue")
+              }
+              else if(res.data == -1) {
+                add_percent = false
+                that.setData({notification_text: "处理完成，正在下载图片"})
+                console.log("ready to download")
+                if(progress_percent < 85) {
+                  progress_percent = 85
+                  that.setData({progressPercent: progress_percent})
+                }
+                percent_sup = 100
+                add_percent = true
+                iscomplete = true
+                wx.downloadFile({
+                  url: "https://" + getApp().globalData.core_url + "/download/?id=" + getApp().globalData.user_ID,
+                  success: function(res) {
+                    that.setData({result_src: res.tempFilePath})
+                    that.setData({progressPercent: 100})
+                    progress_percent = 100
+                    getApp().globalData.result_Img = res.tempFilePath
+                    console.log(res)
+                    that.setData({ getresult: true })
+                    wx.showModal({
+                      title: '提示',
+                      content: '图片处理成功，点击图片可进入预览，预览状态下长按图片可保存',
+                      showCancel: false
+                    })
+                    console.log("finished!")
+                  }
+                })
+              }
+          },
+          fail: function(res) {
+            wx.showToast({
+              title: '无法获取服务器状态！请检查网络连接。',
+              duration: 2000
+            })
+          }
+        })
+      }
+    }, 3000)
+  },
+  nextPage: function (e) {
+    wx.navigateTo({
+      url: '../normal/normal'
+    })
+  },
+
+  //从服务器获得排队信息
+  getQueue: function (e) {
+    console.log("current User ID: " + getApp().globalData.user_ID)
+    wx.request({
+      url: "https://" + getApp().globalData.core_url + "/query/",
+      data: {
+        id: getApp().globalData.user_ID
+      },
+      method: 'GET',
+      success: function (res) {
+        console.log("手动获取排队信息")
+        console.log(res.data)
+        wx.showToast({
+          title: '手动获取排队信息成功',
+        })
+      },
+      fail: function (err) {
+        //console.log("failed to get queue info")
+        //console.log(err)
+      }
+    })
+  },
+
+  //从服务器获得处理完的图片
+  getImage: function (e) {
+    var that = this
+    wx.downloadFile({
+      url: "https://" + getApp().globalData.core_url + "/download/?id=" + getApp().globalData.user_ID,
+      success: function (res) {
+        console.log("download successfully")
+        console.log(res)
+        that.setData({ result_src: res.tempFilePath })
+        getApp().globalData.result_Img = res.tempFilePath
+
+        wx.showToast({
+          title: '尝试手动从服务器获取图片',
+          duration: 3000
+        })
+      },
+      fail: function (err) {
+        //console.log("download failed")
+      }
+    })
+  },
+
+  //预览图片
+  preview_Image: function(e) {
+    var that = this
+    wx.previewImage({
+      urls: [getApp().globalData.result_Img],
+    })
+  },
+
+  lastPage: function(e) {
+    wx.redirectTo({
+      url: '../endpage/endpage',
+    })
+  }
+})
---
+++ pages/login/login.js
@@ -0,0 +1,131 @@
+//登录页面
+const app = getApp()
+
+Page({
+  data: {
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    imgurl: "",
+    imgHeight: 0
+  },
+
+  //事件处理函数
+  bindViewTap: function () {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          //console.log(res) //============================
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+    //console.log(app.globalData.userInfo)
+  },
+  getUserInfo: function (e) {
+    //console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  },
+  //获取用户信息
+  getAllUserInfo: function (e) {
+    var that = this
+    wx.getUserInfo({
+      success: function (res) {
+        var userInfo = res.userInfo //用户基本信息
+        var nickName = userInfo.nickName //用户名
+        var avatarUrl = userInfo.avatarUrl //头像链接
+        //console.log(nickName)
+        //console.log(userInfo)
+        //console.log(avatarUrl)
+        that.setData({imgurl: avatarUrl})
+      }
+    })
+  },
+  lastPage: function(e) {
+    wx.navigateTo({
+      url: '../intro/intro'
+    })
+  },
+  nextPage: function(e) {
+    wx.navigateTo({
+      url: '../normal/normal'
+    })
+  },
+  uploadImg() {
+    var that = this
+    wx.chooseImage({
+      count: 1, // 默认9
+      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+      success(res) {
+        const src = res.tempFilePaths[0]
+        getApp().globalData.selectedImgSrc = res.tempFilePaths[0]
+        wx.navigateTo({
+          url: '../normal/normal'
+        })
+        that.setData({imgurl: res.tempFilePaths[0]})
+      }
+    })
+  },
+  useMyAvatar() {
+    var that = this
+    wx.getUserInfo({
+      success: function (res) {
+        var userInfo = res.userInfo
+        //selectedImgSrc = userInfo.avatarUrl
+        getApp().globalData.selectedImgSrc = userInfo.avatarUrl
+        //console.log("use Avatar!")
+        //console.log(userInfo.avatarUrl)
+        wx.navigateTo({
+          url: '../normal/normal'
+        })
+        that.setData({ imgurl: userInfo.avatarUrl })
+      }
+    })
+  },
+  takephoto() {
+    var that = this
+    wx.chooseImage({
+      count: 1,
+      sizeType: ['original', 'compressed'],
+      sourceType: ['camera'],
+      success(res) {
+        var src = res.tempFilePaths[0]
+        getApp().globalData.selectedImgSrc = src
+        console.log("get the photo taken just now!")
+        wx.navigateTo({
+          url: '../normal/normal',
+        })
+        that.setData({imgurl: src})
+      }
+    })
+  }
+})
